dd69ac18997a974e1b4b0dfa9e2ddf54d45c2bb6
ankeshkushwaha <ankeshkush9651@gmail.com>
2024-11-21 13:46:06 +0530

    create first react app

---
+++ 01-react-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+      <h2>Ankesh Kushwaha</h2>
+      <Youtube></Youtube>
+    </>
+  );
+}
+
+export default App;
---
+++ 01-react-project/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube() {
+  return (
+      <h1>hi! this is my youtube link</h1>
+   )
+}
+
+export default Youtube;
